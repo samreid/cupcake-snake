a4bedefb3b43810af8b7caaf5358d5ba5550ee9a
samreid <reids@colorado.edu>
2015-12-12 13:40:35 -0700

    Display a single node

--- js/cupcake-snake-main.js
+++ js/cupcake-snake-main.js
@@ -5,8 +5,27 @@ define( function( require ) {
 
   // modules
   var SimLauncher = require( 'JOIST/SimLauncher' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Display = require( 'SCENERY/display/Display' );
+  var Text = require( 'SCENERY/nodes/Text' );
+
+  var rootNode = new Node();
+  var display = new Display( rootNode, {
+    allowSceneOverflow: true,
+    isApplication: true,
+    accessibility: false
+  } );
+
+  var simDiv = display.domElement;
+  simDiv.id = 'sim';
+  document.body.appendChild( simDiv );
 
   SimLauncher.launch( function() {
     console.log( 'hello' );
+    var callback = function() {
+      console.log( 'again' );
+      rootNode.addChild( new Text( 'hello', { fill: 'white', centerX: 200, centerY: 200 } ) );
+    };
+    display.updateOnRequestAnimationFrame( callback );
   } );
 } );
