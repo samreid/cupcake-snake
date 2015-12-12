4ef29aabc972d8d54fb2fadca201753ba3f7685a
samreid <reids@colorado.edu>
2015-12-12 13:32:05 -0700

    Added initial scaffolding

---
+++ js/cupcake-snake-main.js
@@ -0,0 +1,12 @@
+// Copyright 2015, University of Colorado Boulder
+
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var SimLauncher = require( 'JOIST/SimLauncher' );
+
+  SimLauncher.launch( function() {
+    console.log( 'hello' );
+  } );
+} );
